be90e87a214187521149d010d8cf7642daf4ccf3
Valentin <miauuuau@gmail.com>
2021-06-16 20:21:14 +0200

    Initial commit

---
+++ Aufgabe4/gta_v4/gta-server.js
@@ -0,0 +1,148 @@
+/**
+ * Template für Übungsaufgabe VS1lab/Aufgabe3
+ * Das Skript soll die Serverseite der gegebenen Client Komponenten im
+ * Verzeichnisbaum implementieren. Dazu müssen die TODOs erledigt werden.
+ */
+
+/**
+ * Definiere Modul Abhängigkeiten und erzeuge Express app.
+ */
+
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app;
+app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Setze ejs als View Engine
+app.set('view engine', 'ejs');
+
+/**
+ * Konfiguriere den Pfad für statische Dateien.
+ * Teste das Ergebnis im Browser unter 'http://localhost:3000/'.
+ */
+
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Konstruktor für GeoTag Objekte.
+ * GeoTag Objekte sollen min. alle Felder des 'tag-form' Formulars aufnehmen.
+ */
+
+function geoTagObject(latitude, longitude, name, hashtag){
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.name = name;
+    this.hashtag = hashtag;
+}
+
+/**
+ * Modul für 'In-Memory'-Speicherung von GeoTags mit folgenden Komponenten:
+ * - Array als Speicher für Geo Tags.
+ * - Funktion zur Suche von Geo Tags in einem Radius um eine Koordinate.
+ * - Funktion zur Suche von Geo Tags nach Suchbegriff.
+ * - Funktion zum hinzufügen eines Geo Tags.
+ * - Funktion zum Löschen eines Geo Tags.
+ */
+
+var geoTagModule = (
+    var tagObjectArray  = [];
+    function nameEquals (tag){
+        return tag.name === name;
+    }
+    function hashtagEquals (tag){
+        return tag.hashtag === hashtag;
+    }
+    function addTag(latitude, longitude, name, hashtag){
+        let geoTag = new geoTagObject(latitude, longitude, name, hashtag);
+        tagObjectArray.push(geoTag);
+    };
+    function deleteTag(name){
+        var foundIndex = tagObjectArray.findIndex(nameEquals);
+        if (foundIndex != -1){
+            tagObjectArray.splice(foundIndex,1);
+        }
+    };
+    function searchByNameOrHashtag(nameOrHashtag) {
+        var foundNameIndex = tagObjectArray.findIndex(nameEquals);
+        var foundHashtagIndex = tagObjectArray.findIndex(hashtagEquals);
+        if (foundNameIndex != -1) {
+            return foundNameIndex;
+        } else if (foundHashtagIndex != -1) {
+            return foundHashtagIndex;
+        }
+    }
+    function searchAtCoords(latitude, longitude, radius) {
+
+    }
+)
+
+/**
+ * Route mit Pfad '/' für HTTP 'GET' Requests.
+ * (http://expressjs.com/de/4x/api.html#app.get.method)
+ *
+ * Requests enthalten keine Parameter
+ *
+ * Als Response wird das ejs-Template ohne Geo Tag Objekte gerendert.
+ */
+
+app.get('/', function(req, res) {
+    res.render('gta', {
+        taglist: []
+    });
+});
+
+/**
+ * Route mit Pfad '/tagging' für HTTP 'POST' Requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests enthalten im Body die Felder des 'tag-form' Formulars.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Mit den Formulardaten wird ein neuer Geo Tag erstellt und gespeichert.
+ *
+ * Als Response wird das ejs-Template mit Geo Tag Objekten gerendert.
+ * Die Objekte liegen in einem Standard Radius um die Koordinate (lat, lon).
+ */
+
+// TODO: CODE ERGÄNZEN START
+
+/**
+ * Route mit Pfad '/discovery' für HTTP 'POST' Requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
+ *
+ * Requests enthalten im Body die Felder des 'filter-form' Formulars.
+ * (http://expressjs.com/de/4x/api.html#req.body)
+ *
+ * Als Response wird das ejs-Template mit Geo Tag Objekten gerendert.
+ * Die Objekte liegen in einem Standard Radius um die Koordinate (lat, lon).
+ * Falls 'term' vorhanden ist, wird nach Suchwort gefiltert.
+ */
+
+// TODO: CODE ERGÄNZEN
+
+/**
+ * Setze Port und speichere in Express.
+ */
+
+var port = 3000;
+app.set('port', port);
+
+/**
+ * Erstelle HTTP Server
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Horche auf dem Port an allen Netzwerk-Interfaces
+ */
+
+server.listen(port);
